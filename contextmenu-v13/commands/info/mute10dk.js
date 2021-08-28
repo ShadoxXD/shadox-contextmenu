da7c0d64f0d4dec3e18ac92db6778e78e4de014a
shadox <84858225+ShadoxXD@users.noreply.github.com>
2021-08-28 18:44:39 +0300

    contextmenu x)

--- contextmenu-v13/commands/info/mute10dk.js
+++ contextmenu-v13/commands/info/mute10dk.js
@@ -39,7 +39,7 @@ module.exports = {
         let channel = interaction.guild.channels.cache.get(client.config.logs.mutelog)
         let embed = new MessageEmbed().setAuthor(client.config.embed.sunucuAdı, interaction.guild.iconURL({dynamic: true}))
         .setDescription(`${user} kullanıcısı metin kanallarında ${interaction.user} tarafından **10 dakika** boyunca susturulmuştur.\n\nMute Atılış Tarihi: \`${moment(Date.now()).format("LLL")}\`\nMute Bitiş Tarihi: \`${moment(Date.now() + ms("10m")).format("LLL")}\`\n Ceza Puanı \`(#${veri})\``)
-        .setFooter(client.config.embed.authorTag)
+        .setFooter(client.config.embed.Footer)
         .setColor("RANDOM")
         channel.send({ embeds: [embed]})
 
--- contextmenu-v13/config.js
+++ contextmenu-v13/config.js
@@ -1,27 +1,27 @@
 module.exports = {
-    token: "ODgwODA1MjM5MjMyNTU3MTE3.YSjnzQ.BY9x1WOeQM4LLrbH_tMEYXstXHI",
-    prefx: "!",
-    owner: "860119403978817546",
-    mongoURL: "mongodb+srv://shadoxdb:tevfik01@cluster0.0t0hi.mongodb.net/veritabani?retryWrites=true&w=majority",
-    sunucuID: "880502244431261696",
+    token: "",
+    prefix: "",
+    owner: "",
+    mongoURL: "",
+    sunucuID: "",
     
     roles: {
         muted: "880503212375961620",
     }, 
 
     embed: {
-        sunucuAdı: "C5 #LOADİNG",
-        embedRenk: "RANDOM",
-        authorTag: "C5 #LOADİNG",
+        sunucuAdı: "",
+        embedRenk: "",
+        Footer: "",
     },
 
     yetki: {
      
-        muteStaff: "880503221783756801",
+        muteStaff: "",
     },
 
     logs:{
-        mutelog: "880503311919353986",
+        mutelog: "",
 
     },
     
